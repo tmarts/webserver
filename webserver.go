c18b9c0178a09bce67c610ac4fe97502a76a026a
AndyClaiborne <andy@whitecheetah.com>
2018-02-21 20:03:39 -0700

    Run on the port specified by the PORT env var

--- webserver.go
+++ webserver.go
@@ -4,10 +4,15 @@ import (
 	"github.com/veloandy/webserver/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", handlers.HomePageHandler)
 	http.HandleFunc("/viewxml", handlers.XMLHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
